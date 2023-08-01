54632e72bd46ab81211320b3eb74f250474e1b2a
Yan Aung Hein <yanaunghein311@gmail.com>
2023-08-01 14:42:17 +0630

    download base and bars in zip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,29 @@
 <script>
-import Error from './+error.svelte';
+import { onMount } from 'svelte';
+import { gsap } from 'gsap';
+import { Draggable } from 'gsap/dist/Draggable';
+import JSZip from 'jszip';
 
-let images;
+onMount(() => {
+  gsap.registerPlugin(Draggable);
+});
+
+let px = 1;
+let images = [];
+let realImages = [];
+let name = '';
+
+const displayWidth = 720;
+
+const setDisplayDimention = (canvas, naturalWidth, naturalHeight) => {
+  if (naturalWidth > 500) {
+    canvas.width = displayWidth;
+    canvas.height = displayWidth * (naturalHeight / naturalWidth);
+  } else {
+    canvas.width = naturalWidth;
+    canvas.height = naturalHeight;
+  }
+};
 
 const convertToBase64 = async (files) => {
   const filePromises = Array.from(files).map((file) => {
@@ -19,64 +41,144 @@ const convertToBase64 = async (files) => {
   return results;
 };
 
-const generateBaseImage = (node) => {
+const transformToActualImages = (base64Images) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const images = [];
+      for (const image of base64Images) {
+        const actualImage = document.createElement('img');
+        actualImage.src = image;
+        images.push(actualImage);
+      }
+      resolve(images);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const drawBars = () => {
+  const bars = document.getElementById('bars');
+  const imagesCount = images.length;
+  const barsCount = Math.ceil(bars.width / imagesCount);
+
+  const ctx = bars.getContext('2d');
+  for (const i of [...Array(barsCount).keys()]) {
+    ctx.fillStyle = '#000';
+    ctx.fillRect(i * imagesCount, 0, imagesCount - px, realImages[0].naturalHeight);
+  }
+};
+
+const generateBaseImage = async (node) => {
   node.addEventListener('change', async () => {
     const { files } = node;
     if (files.length === 0) return;
 
     const base64Images = await convertToBase64(files);
     images = base64Images;
-    console.log(base64Images);
+
+    const imagesX = await transformToActualImages(images);
+    realImages = imagesX;
+
+    const base = document.getElementById('base');
+    const bars = document.getElementById('bars');
+
+    const imgW = imagesX[0].naturalWidth;
+    const imgH = imagesX[0].naturalHeight;
+
+    base.width = imgW;
+    base.height = imgH;
+
+    bars.width = imgW;
+    bars.height = imgH;
+
+    const ctx = base.getContext('2d');
+
+    const sliceSequence = [...Array(1000).keys()].map((i) => i * px * imagesX.length);
+    for (const [index, image] of imagesX.entries()) {
+      for (const i of sliceSequence) {
+        ctx.drawImage(image, i + px * index, 0, px, imgH, i + px * index, 0, px, imgH);
+      }
+    }
+
+    drawBars();
+
+    const displayContainer = document.getElementById('display-base');
+    const displayBase = document.getElementById('display-base');
+    const displayBars = document.getElementById('display-bars');
+
+    setDisplayDimention(displayContainer, imgW, imgH);
+    setDisplayDimention(displayBase, imgW, imgH);
+    setDisplayDimention(displayBars, imgW, imgH);
+
+    const displayBaseCtx = displayBase.getContext('2d');
+    const displayBarsCtx = displayBars.getContext('2d');
+
+    displayBaseCtx.drawImage(base, 0, 0, base.width, base.height, 0, 0, displayBase.width, displayBase.height);
+    displayBarsCtx.drawImage(bars, 0, 0, bars.width, bars.height, 0, 0, displayBars.width, displayBars.height);
+
+    Draggable.create('#display-bars', { type: 'x,y', edgeResistance: 0, bounds: '#display-container' });
   });
 };
 
-const setBackground = (node, data) => {
-  const { image, index } = data;
+const downloadImage = (node) => {
+  node.addEventListener('click', () => {
+    if (name === '') {
+      alert('Please enter your scanimation name.');
+      return;
+    }
 
-  node.style.background = `linear-gradient(to right, white 0, white 7px, transparent 7px, transparent 8px), url(${image})`;
-  node.style.backgroundSize = '8px 100%, contain';
-  node.style.backgroundPosition = '0px 0px 0px 0px';
-  node.style.backgroundRepeatY = 'no-repeat';
+    const zip = new JSZip();
+    const base = document.getElementById('base');
+    const bars = document.getElementById('bars');
 
-  if (index === 0) {
-    node.style.backgroundPosition = '0px 0px';
-  } else {
-    node.style.backgroundPositionX = `${1 * index}px`;
-  }
-};
+    for (const canvas of [base, bars]) {
+      zip.file(`${canvas.dataset.name}.png`, canvas.toDataURL('image/png').split('base64,')[1], { base64: true });
+    }
 
-const play = () => {
-  document.getElementsByClassName('cover')[0].classList.toggle('opacity-0');
-};
-
-function downloadimage() {
-  var node = document.getElementById('base-image');
-
-  fetch('/api/generate-image', { method: 'POST', body: JSON.stringify({ html: node.outerHTML }) })
-    .then((resp) => resp.json())
-    .then((data) => {
-      const img = new Image();
-      img.src = data.image;
-      document.body.appendChild(img);
-      console.log(data.image);
+    zip.generateAsync({ type: 'blob' }).then((content) => {
+      const downloadLink = document.createElement('a');
+      downloadLink.href = URL.createObjectURL(content);
+      downloadLink.download = `${name}-scanimation.zip`;
+      downloadLink.click();
     });
-}
+  });
+};
 </script>
 
-<input use:generateBaseImage type="file" accept="image/*" multiple />
-<button on:click={play}>Play</button>
-<button on:click={downloadimage}>Download</button>
+<div class="overflow-hidden min-h-screen w-full p-10 flex flex-col items-center">
+  <label
+    for="select-files"
+    class="bg-sky-600 w-[200px] h-10 text-white rounded-full cursor-pointer grid place-items-center"
+  >
+    <input use:generateBaseImage type="file" multiple id="select-files" class="hidden" />
+    <span>Select Frames</span>
+  </label>
 
-{#if images}
-  <div id="base-image" class="relative w-96 h-96">
-    <div class="w-full h-full">
-      {#each images as image, index}
-        <h1>Hello</h1>
-        <!-- <img src={image} alt="" class="border border-red-500 absolute mix-blend-multiply" /> -->
-        <!-- <div use:setBackground={{ image, index }} class="absolute inset-0 mix-blend-multiply" /> -->
-      {/each}
-    </div>
-
-    <div class="cover absolute z-50 inset-0 opacity-0" />
+  <div class="hidden">
+    <canvas data-name="base" id="base" />
+    <canvas data-name="bars" id="bars" />
   </div>
-{/if}
+
+  <div id="display-container" class="relative w-full h-full mt-10 flex items-center justify-center">
+    <canvas data-name="base" id="display-base" />
+    <canvas data-name="bars" id="display-bars" class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+  </div>
+
+  {#if images.length > 0}
+    <div class="flex items-center justify-center gap-2 mt-10">
+      <input
+        bind:value={name}
+        type="text"
+        placeholder="Scanimation Name..."
+        class="border border-sky-600 h-10 flex items-center outline-none rounded-full px-3 w-[12rem]"
+      />
+      <button
+        use:downloadImage
+        class="appearance-none bg-sky-600 w-[120px] h-10 text-white rounded-full cursor-pointer"
+      >
+        Donwload
+      </button>
+    </div>
+  {/if}
+</div>
